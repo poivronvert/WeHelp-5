ebe54f61710575a55166605f66ef4b0e7f419d86
AliceKuo <ming-chieh.kuo@outlook.com>
2024-04-18 18:18:10 +0800

    task 2 completed

--- week3/assignment-3.py
+++ week3/assignment-3.py
@@ -1,8 +1,11 @@
+# Task 1
+
 from urllib import request
 from collections import OrderedDict
 import json
 import re
 import csv
+
 
 
 SRC_URL_1:str = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
@@ -61,7 +64,7 @@ merged_list = merged_dict.values()
 csv_file = "spot.csv"
 fieldnames = ["SpotTitle","District","Longitude","Latitude","ImageURL"]
 
-with open(csv_file, 'w', newline='',encoding="utf-8") as file:
+with open(csv_file, 'w', newline='',encoding="cp950") as file:
     writer = csv.DictWriter(file, fieldnames = fieldnames)
     # 寫入csv的標題(column)
     writer.writeheader()
@@ -94,7 +97,7 @@ for item in merged_list:
 # 寫入到csv
 mrt_csv_file = "mrt.csv" #建立一個mrt.csv
 
-with open(mrt_csv_file,'w',newline="",encoding="utf-8") as file:
+with open(mrt_csv_file,'w',newline="",encoding="cp950") as file: # 編碼要用cp950，excel才不會顯示亂碼
     fieldnames = ["MRT","stitle"]
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
@@ -103,24 +106,84 @@ with open(mrt_csv_file,'w',newline="",encoding="utf-8") as file:
         writer.writerow({'MRT':station, "stitle":", ".join(spots)})
 
 
+# Task 2
+# 抓取PTT Lottery的網頁原始碼(HTML)
+import urllib.request as req
+import re
+import logging
+from logging.config import dictConfig
+import yaml
+
+LOG_CONF:str = "./logging.yaml"
+
+def init_logger()->logging.Logger:
+     with open(LOG_CONF, 'r') as f:
+         conf = yaml.safe_load(f)
+     dictConfig(conf)
+     return logging.getLogger()
+
+log = init_logger()
+
+def get_data(url):
+     #建立一個Request物件，附加Request Headers的資訊(模擬人類的request)
+
+     request=req.Request(url, headers={
+         "cookie":"over18=1", # 觀察連線得知藉由cookie over18=1之後會導入頁面
+         "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+     }) # User-Agent的字串可以打開瀏覽器->Inspect->Network->Name->Headers->User-Agent中找到
+
+     with req.urlopen(request) as response:
+         data = response.read().decode("utf-8")
+
+     # 解析原始碼，取得每篇文章的標題
+     import bs4
+     root = bs4.BeautifulSoup(data, "html.parser") # 讓Beautiful協助我們解析HTML文件
+
+     titles = root.find_all("div",class_="title") # 尋找class="title"的div標籤
+     likes_dislikes = root.find_all("div", class_="nrec") # 尋找class="nrec"的div標籤
+
+     result_list = []
+     # 使用 zip 函數將 titles、likes_dislikes 和 dates 合併在一起
+     for title,like_dislike in zip(titles,likes_dislikes):
+         if title.a != None:
+             title_text = title.a.string # 如果有標題包含a標籤（沒有被刪除），印出來
+             title_url = title.a["href"]
+             title_request = req.Request("https://www.ptt.cc"+title_url, headers={
+             "cookie":"over18=1", # 觀察連線得知藉由cookie over18=1之後會導入頁面
+             "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+         }) # User-Agent的字串可以打開瀏覽器->Inspect->Network->Name->Headers->User-Agent中找到
+
+             with req.urlopen(title_request) as title_response:
+                 title_data = title_response.read().decode("utf-8")
+             title_root = bs4.BeautifulSoup(title_data,"html.parser")
+
+             def match_regex(text):
+                 return re.match(r'\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}', str(text))
+             date_text = title_root.find(text=match_regex)
+
+             like_dislike_text = like_dislike.span.string if like_dislike.string is not None else None
+             result_list.append((title_text, like_dislike_text, date_text))
+     for item in result_list:
+         log.info(item)
+
+     # 找到下一頁的連結
+     nextlink = root.find("a", string="‹ 上頁") # 找到內文是‹ 上頁的 a 標籤
+     return (nextlink["href"]) # 印出href的網址，還要再手動加上https://的prefix
 
 
+def main():
+     # 主程序：抓取多個頁面的標題
+     try:
+         page_url = "https://www.ptt.cc/bbs/Lottery/index.html"
+         count = 0
+         while count < 3:
+             page_url = "https://www.ptt.cc"+ get_data(page_url) #手動加上https://的prefix
+             count += 1
+         log.info(page_url)
+     except Exception as e:
+         log.error(e, exc_info=True)
+
+if __name__ == '__main__':
+     main()
 
 
-
-
-
-
-
-
-
-
-    
- 
-    
-
-
-
-
-
-        
