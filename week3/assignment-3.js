560f20f53bb47ebbe24e67133b5fd76b5d948291
AliceKuo <ming-chieh.kuo@outlook.com>
2024-04-24 18:13:37 +0800

    update css layout

--- week3/assignment-3.js
+++ week3/assignment-3.js
@@ -1,6 +1,5 @@
 const URL = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
 var currentCount = 13;
-
 /**
  * fetch data from the specified URL and render spot information based on startIndex
  * @param {Number} startIndex The start index of spots to render
@@ -13,52 +12,22 @@ function getData(startIndex, count){
         })
         .then(function(resp) {
             let results = resp.data.results;
-            
-            
-        for (let i=startIndex; i<startIndex+count;i++){
-            if (i>=results.length) {
-                let loadMoreButton = document.querySelector("#load");
-                loadMoreButton.style.display = "none";
-                return;
-            }
-            let result =results[i]
-            let className = determineClassName(i);
+            for (let i=startIndex; i<startIndex+count;i++){
+                if (i==results.length) {
+                    let loadMoreButton = document.querySelector("#load");
+                    loadMoreButton.style.display = "none";
+                    break;
+                }
+                let result =results[i]
+                if (i <3){
+                    appendSpot(result,".first3Spots")
+                }else{
+                    appendSpot(result,".otherSpots")
+                }
 
-            appendSpot(result,className)
-
-            }
-        if (startIndex!==0)currentCount += count;
-        
+                }
+            if (startIndex!==0)currentCount += count;
         });
-}
-/**
- * Determine the className based on the current viewport width and index
- * @param {number} i The index of spots
- * @returns The determined className
- */
-function determineClassName(i){
-    const width = window.innerWidth;
-    if (width >600 && width <=1200){
-        if (i<2) {
-            return 'sb';
-        } else if (i===2) {
-            return 'sb2';
-        } else if ((i%10===1)||(i%10===2)) {
-            return 'lb';
-        } else {
-            return 'mb';
-        }
-    // console.log("601px < width <=1200px")
-    } else{
-        if (i<3) {
-            return 'sb';
-        }else if((i===3) || (i-3)%5===0){
-            return 'lb';
-        }else{
-            return 'mb';
-        }
-    // console.log("其他範圍")
-    }
 }
 
 /**
@@ -66,43 +35,31 @@ function determineClassName(i){
  * @param {list} result The fetched data of spots
  * @param {str} className The determined clasName for styling
  */
-function appendSpot(result,className){
+function appendSpot(result,place){
 
     let parentDiv = document.createElement('div');
-    parentDiv.className = className;
+    parentDiv.className = "spot";
     
-
+    
     let spotImage = document.createElement('img');
     spotImage.src = "https://" + result.filelist.split("https://")[1];
-
-
     parentDiv.appendChild(spotImage);
-
 
     let spotName = document.createElement('div');
     spotName.textContent = result.stitle;
-
-
     parentDiv.appendChild(spotName);
 
-    if(className === "lb"|| className === "mb"){
+    if (place == ".otherSpots"){ 
         let starImage = document.createElement('img');
         starImage.src = "star1.png";
         starImage.className = "star";
         parentDiv.appendChild(starImage);
     }
 
-
-    let container = document.querySelector(".content .container");
+    let container = document.querySelector(place);
     container.appendChild(parentDiv);
     
 }
-
-window.addEventListener('resize', function() {
-    let container = document.querySelector(".content .container");
-    container.innerHTML = '';
-    getData(0, currentCount);
-});
 
 document.addEventListener('DOMContentLoaded',function(){
     getData(0,currentCount);
